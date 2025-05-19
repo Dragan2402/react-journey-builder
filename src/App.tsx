18fad909ce8a57c3148bc3850ca039a5f247cee0
Dragan Mirkovic <draganmirkovic2402@gmail.com>
2025-05-19 11:49:54 +0200

    Added react flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import GraphFlow from './components/react-flow/ReactFlow';
+
 function App() {
 	return (
-		<>
-			<div>Hello</div>
-		</>
+		<div className="flex bg-white item-center h-full w-full">
+			<GraphFlow />
+		</div>
 	);
 }
 
